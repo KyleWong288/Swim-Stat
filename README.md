86ff3d28883d86f2a844e263d4f2354d75e8eac8
Kyle Wong <knw@ucsb.edu>
2022-09-15 15:53:35 -0700

    added .env

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Swim-Stat
+# SwimStat
+
+Website that predicts the future times of a swimmer.
+Applies linear regression (specifically least squares method to a logarithmic model) for most accurate results.
+Built with MERN.
+
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 
 const SwimModel = require("./models/swim-schema");
 
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    "mongodb+srv://Kylewong:Kylewong1@crud.imbs0ja.mongodb.net/swim?retryWrites=true&w=majority", 
+    process.env.MONGOCONNECT, 
     {
         useNewUrlParser: true,
     }
